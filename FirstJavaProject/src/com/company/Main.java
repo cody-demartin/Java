cd26c48669d58eec822c17dd364d6a013b68b21d
cdemarti <codythomasdemartin@gmail.com>
2021-03-04 14:22:35 -0500

    completes variable section of java course

--- FirstJavaProject/src/com/company/Main.java
+++ FirstJavaProject/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
