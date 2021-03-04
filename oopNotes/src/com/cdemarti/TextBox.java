a4d1955db7b88ecbed793513a425301eadd73d74
cdemarti <codythomasdemartin@gmail.com>
2021-03-04 17:33:34 -0500

    creates textfield class

---
+++ oopNotes/src/com/cdemarti/TextBox.java
@@ -0,0 +1,13 @@
+package com.cdemarti;
+
+public class TextBox {
+    public String text; // field
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        this.text = "";
+    }
+}
