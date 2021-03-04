812c358e187157246437a46ae46adfa82580b84f
cdemarti <codythomasdemartin@gmail.com>
2021-03-04 15:57:08 -0500

    adds logical operator notes

--- controlFlowNotes/src/com/cdemarti/Main.java
+++ controlFlowNotes/src/com/cdemarti/Main.java
@@ -7,5 +7,15 @@ public class Main {
 	    int y = 1;
         System.out.println(x == y); // boolean expression
         System.out.println(x != y);
+
+        int temp = 12;
+        boolean isWarm = temp > 20 && temp < 30;
+        System.out.println(isWarm);
+
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+        System.out.println(isEligible);
     }
 }
