c00c21d65ec4d84b07afa2691f63314458eb814a
cdemarti <codythomasdemartin@gmail.com>
2021-03-04 17:55:51 -0500

    refactored to use getter and setter methods to enforce encapsulation

--- proceduralToOOP/src/com/cdemarti/Employee.java
+++ proceduralToOOP/src/com/cdemarti/Employee.java
@@ -1,10 +1,32 @@
 package com.cdemarti;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Salary cannot be zero or less.");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0) {
+            throw new IllegalArgumentException("Rate cannot be zero or less.");
+        }
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
     }
 }
