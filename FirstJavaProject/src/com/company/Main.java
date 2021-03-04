46701eb9d288550b6e88b65b21ef8848b789801a
cdemarti <codythomasdemartin@gmail.com>
2021-03-04 14:28:11 -0500

    completes primitive type section

--- FirstJavaProject/src/com/company/Main.java
+++ FirstJavaProject/src/com/company/Main.java
@@ -3,8 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
+        byte myAge = 30;
+        long viewsCount = 3_123_456_789L; // add L to long numbers
+        float price = 10.99F; // add F to floats
+        char letter = 'A'; // single char with single, strings with double
+        boolean isEligible = false;
         System.out.println(herAge);
     }
 }
+
+// primitive types
+//  for storing simple values
+//      bytes 1 byte [-128, 127]
+//      short 2 bytes [-32k, 32k]
+//      int   4 bytes [-2B, 2B]
+//      long  8 bytes even larger
+//      float 4 bytes
+//      double 8 bytes
+//      char   2 bytes
+//      boolean 1 byte true/false
+
