ec37c5ecf9264057b8042ed0fad232dd7b73596a
cdemarti <codythomasdemartin@gmail.com>
2021-03-04 17:49:21 -0500

    refactos procedural to OOP

---
+++ proceduralToOOP/src/com/cdemarti/Employee.java
@@ -0,0 +1,10 @@
+package com.cdemarti;
+
+public class Employee {
+    public int baseSalary;
+    public int hourlyRate;
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+}
